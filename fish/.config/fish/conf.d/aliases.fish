bf60a72b164ace8a058579cf88e5986837db8665
futantan <luckytantanfu@gmail.com>
2026-02-25 07:54:45 +1100

    alias

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,10 +1,9 @@
-alias cz git-cz
-alias dev 'npm run dev'
-alias gcmsg 'git commit -m'
-alias ll 'exa -l -g --icons'
-alias lla 'll -a'
-alias o 'open .'
-alias p pnpm
-alias z __zoxide_z
-alias zi __zoxide_zi
+alias cz=git-cz
+alias dev='npm run dev'
+alias gcmsg='git commit -m'
+alias ll='exa -l -g --icons'
+alias lla='ll -a'
+alias o='open .'
+alias p=pnpm
 
+alias sshdokploy='ssh root@100.65.70.11'
