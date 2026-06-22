9cc2fd15b6cbeb0bc06c520752c4087659ef2273
Tantan Fu <luckytantanfu@gmail.com>
2026-06-22 22:30:09 +1000

    feat(nvim): support SVG image previews

---
+++ nvim/.config/nvim/lua/plugins/snacks-image.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {
+        enabled = true,
+        formats = {
+          "png",
+          "jpg",
+          "jpeg",
+          "gif",
+          "bmp",
+          "webp",
+          "tiff",
+          "heic",
+          "avif",
+          "pdf",
+          "svg",
+        },
+        doc = {
+          enabled = true,
+          inline = false,
+          float = true,
+          max_width = 80,
+          max_height = 40,
+        },
+      },
+    },
+  },
+}
